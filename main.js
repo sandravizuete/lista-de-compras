1f6c416022a74e310680e929ac3781539d7fce85
Sandra <sandravizueteperez@gmail.com>
2016-12-20 16:13:29 +0100

    implementada toda la funcionalidad

--- main.js
+++ main.js
@@ -11,11 +11,26 @@ $(document).ready(function(e) {
   });
 
   $("ul").on('click', 'li', function() {
-    $(this).toggleClass('active');
-
     var badge = $(this).children('.badge');
     var contador = parseInt(badge.html());
-    badge.html(contador + 1);
+    if(contador == 0){
+      badge.html(contador + 1);
+    }else{
+      badge.html('0');
+    }
 
+    $(this).toggleClass('active');
+
+  });
+
+  $('ul').on('click', 'button', function(event) {
+    var badge = $(this).siblings('.badge');
+    var contador = parseInt(badge.html());
+    if(contador == 0){//hacemos que cuando este acero el contador y le demos lo ponga azul
+      $(this).parent('li').addClass('active');
+    }
+    badge.html(contador + 1);
+    event.preventDefault();
+    return false;//hacemos que no salte la otra funcion y no se pinte azul al clicar en el button del carrito
   });
 });
